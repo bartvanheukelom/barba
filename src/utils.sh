63067a8e7c9e5ce270360675d4fcc434b7663e3e
Bart van Heukelom <mail@bartvh.nl>
2019-12-13 14:40:12 +0100

    rc_setup utils

--- src/utils.sh
+++ src/utils.sh
@@ -34,11 +34,13 @@ debug() {
     # preserve the last return code
     local incode="$?"
     
-    echo2 -n "DEBUG: $1"
-    if [[ "$incode" != "0" ]]; then
-        echo2 -n " (also, exit code is $incode)"
+    if "${DEBUG:-"false"}"; then
+        echo2 -n "DEBUG: $1"
+        if [[ "$incode" != "0" ]]; then
+            echo2 -n " (also, exit code is $incode)"
+        fi
+        echo2
     fi
-    echo2
 
     return "$incode"
 }
@@ -59,6 +61,39 @@ set_strict_mode() {
     set -o errexit \
         -o nounset \
         -o pipefail
+}
+
+unset_strict_mode() {
+    set +o errexit \
+        +o nounset \
+        +o pipefail
+}
+
+# rc_setup $cmd [$args...]
+# in an RC file, run $cmd in a mode that behaves much like a normal script
+rc_setup() {
+    rc_setup_enter
+    local rc
+    "$@" && rc=$? || rc=$?
+    rc_setup_leave && return $rc
+}
+
+# in an RC file, enter a mode that behaves much like a normal script
+rc_setup_enter() {
+    
+    # during setup, exit on errors
+    set_strict_mode
+
+    # it appears that if an rcfile is interrupted, normal bash behaviour
+    # is to quit the rcfile but then present the prompt. we don't want that
+    trap 'error Interrupted 130' SIGINT
+
+}
+
+# leave the mode entered by rc_setup_enter, to restore default interactive shell behaviour
+rc_setup_leave() {
+    trap - SIGINT
+    unset_strict_mode
 }
 
 # cd to the directory of the main script
