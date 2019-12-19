7a02e466cd173c5aed5c5d0bce76cae2365813dd
Bart van Heukelom <mail@bartvh.nl>
2019-12-19 15:51:32 +0100

    git: detached head stuff

--- src/git/status.sh
+++ src/git/status.sh
@@ -9,22 +9,53 @@ git.list_repos() {
     git submodule foreach --recursive --quiet 'echo "$toplevel/$path"'
 }
 
-# git.status_if_dirty $repo
-#     prints the status of $repo only if it's not clean
+# prints the status of the current git repo only if it's not clean
 git.status_if_dirty() {
-    (
-        set_strict_mode
-        local path="$1"
-        cd "$path"
-        short=$(git status --short)
-        if [[ "$short" != "" ]]; then
-            echo "######################### $path ############################"
-            git status
-            echo
-            echo
-            echo
+    if git.dirty || git.detached; then
+        echo "######################### $(pwd) ############################"
+        
+        if git.dirty; then
+            # this also prints detached HEAD if applicable
+            git status || return
+        elif git.detached; then
+            echo "detached HEAD @ $(git.current_commit --short), could be attached to:"
+            git.current_refs | git.local_branch_refs | prefix '  - ' || return
         fi
 
-        # TODO alert detached HEAD
-    )
+        echo
+        echo
+    fi
+}
+
+git.dirty() {
+    short=$(git status --short)
+    [[ "$short" != "" ]]
+}
+
+git.current_branch() {
+    local branch
+    branch="$(git symbolic-ref HEAD 2>/dev/null)" || branch_name="(detached HEAD)"
+    branch=${branch#"refs/heads/"}
+}
+
+git.detached() {
+    ! git symbolic-ref HEAD &>/dev/null
+}
+
+git.current_commit() {
+    git rev-parse "$@" HEAD || fun_error
+}
+
+git.current_refs() {
+    git show-ref | { grep "$(git.current_commit)" || true; } | cut -d' ' -f2 || fun_error
+}
+
+git.local_branch_refs() {
+    local branch
+    while IFS= read -r ref; do
+        branch=${ref#"refs/heads/"}
+        if [[ "$branch" != "$ref" ]]; then
+            echo "$branch"
+        fi
+    done
 }
